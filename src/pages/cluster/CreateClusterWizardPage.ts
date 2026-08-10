4e15c1d2ac415e2c708937e10c09defc2eb5f2a4
David Huynh <dhuynh@redhat.com>
2026-08-10 04:49:36 -0700

    updating based on review
    Signed-off-by: David Huynh <dhuynh@redhat.com>

--- src/pages/cluster/CreateClusterWizardPage.ts
+++ src/pages/cluster/CreateClusterWizardPage.ts
@@ -50,9 +50,8 @@ export class CreateClusterWizardPage extends BasePage {
       await this.getHostedCard().click();
       await expect(this.page).not.toHaveURL(/control-plane/, { timeout: 30_000 });
     } else {
-      const standaloneCard = this.getStandaloneCard();
-      await expect(standaloneCard).toBeVisible({ timeout: 15_000 });
-      await standaloneCard.click();
+      await expect(this.page).toHaveURL(/control-plane/, { timeout: 15_000 });
+      await this.getStandaloneCard().click();
     }
 
     await this.waitForLoad();
